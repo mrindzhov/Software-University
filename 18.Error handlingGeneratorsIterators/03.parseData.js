eab3d42bf29178e9c5dbc37f6f8e1756396f7ccf
Martin <mrindzhov@gmail.com>
2016-11-10 11:29:19 +0200

    Update 03.parseData.js

--- 18.Error handlingGeneratorsIterators/03.parseData.js
+++ 18.Error handlingGeneratorsIterators/03.parseData.js
@@ -1,3 +1,59 @@
+function makeCandy(arr) {
+    class Candy {
+        constructor(topping, filling, spice) {
+            this.topping = topping;
+            this.filling = filling;
+            this.spice = spice;
+        }
+    }
+    let result = [];
+    let fillingOptions = ['hazelnut', 'caramel', 'strawberry', 'blueberry', 'yogurt', 'fudge', ''];
+    let toppingOptions = ['milk chocolate', 'white chocolate', 'dark chocolate'];
+    // console.log(arr);
+    for (let element of arr) {
+            element = element.split(':');
+        if (element.length==3) {
+            let isValid = true;
+            let topping = element[0];
+            if (!toppingOptions.includes(topping)) {
+                isValid = false;
+            }
+
+            let filling = element[1].split(',');
+            if (!fillingOptions.includes(...filling) || filling.length > 3) {
+                isValid = false;
+            }
+            if (filling == "") {
+                filling = null;
+            }
+            else {
+                filling = filling.join(',');
+            }
+
+            let spice = element[2];
+            if (spice == 'poison' || spice == 'asbestos') {
+                isValid = false;
+            }
+            if (spice == '') {
+                spice = null;
+            }
+            if (isValid) {
+                result.push(new Candy(topping, filling, spice));
+            }
+        }
+
+    }
+    return result;
+}
+/*
+console.log(makeCandy([
+    'milk chocolate:hazelnut,caramel:pumpkin',
+    'dark chocolate::chips',
+    '',
+    'white chocolate::poison',  // invalid
+    'white chocolate:fudge:',
+    'frosting:yogurt:frosting', // invalid
+    'dark chocolate:blueberry:rock crystals']));*/
 /*function makeCandy(arr) {
     class Candy {
         constructor(topping, filling, spice) {
@@ -65,59 +121,3 @@
     }
     return result;
 }*/
-function makeCandy(arr) {
-    class Candy {
-        constructor(topping, filling, spice) {
-            this.topping = topping;
-            this.filling = filling;
-            this.spice = spice;
-        }
-    }
-    let result = [];
-    let fillingOptions = ['hazelnut', 'caramel', 'strawberry', 'blueberry', 'yogurt', 'fudge', ''];
-    let toppingOptions = ['milk chocolate', 'white chocolate', 'dark chocolate'];
-    // console.log(arr);
-    for (let element of arr) {
-            element = element.split(':');
-        if (element.length==3) {
-            let isValid = true;
-            let topping = element[0];
-            if (!toppingOptions.includes(topping)) {
-                isValid = false;
-            }
-
-            let filling = element[1].split(',');
-            if (!fillingOptions.includes(...filling) || filling.length > 3) {
-                isValid = false;
-            }
-            if (filling == "") {
-                filling = null;
-            }
-            else {
-                filling = filling.join(',');
-            }
-
-            let spice = element[2];
-            if (spice == 'poison' || spice == 'asbestos') {
-                isValid = false;
-            }
-            if (spice == '') {
-                spice = null;
-            }
-            if (isValid) {
-                result.push(new Candy(topping, filling, spice));
-            }
-        }
-
-    }
-    return result;
-}
-/*
-console.log(makeCandy([
-    'milk chocolate:hazelnut,caramel:pumpkin',
-    'dark chocolate::chips',
-    '',
-    'white chocolate::poison',  // invalid
-    'white chocolate:fudge:',
-    'frosting:yogurt:frosting', // invalid
-    'dark chocolate:blueberry:rock crystals']));*/
