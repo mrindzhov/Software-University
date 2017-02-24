c5db55bb32c275397a6fbbd019ef4b2c2077bfb4
Martin <mrindzhov@gmail.com>
2017-02-24 22:12:08 +0200

    Add files via upload

---
+++ 02.IntroductionToEntityFramework/GringottsDatabase/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GringottsDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new GringottsContext();
+            StringBuilder content = new StringBuilder();
+            #region//First Letter
+            //var uniqueFirstLetterGringotts = context.WizzardDeposits
+            //.Where(d => d.DepositGroup == "Troll Chest")
+            //.Select(d => d.FirstName.Substring(0, 1))
+            //.Distinct()
+            //.OrderBy(d => d);
+            //foreach (var letters in uniqueFirstLetterGringotts)
+            //{
+            //    content.AppendLine($"{letters}");
+            //}
+            #endregion
+
+            Console.WriteLine(content);
+        }
+    }
+}
