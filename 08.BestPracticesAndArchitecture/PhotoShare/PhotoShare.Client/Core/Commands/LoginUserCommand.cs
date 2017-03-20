df70141d02fe96a6d828479c5a1d5a72c6d6048a
Martin <mrindzhov@gmail.com>
2017-03-20 11:43:29 +0200

    Added Login and Logout

---
+++ 08.BestPracticesAndArchitecture/PhotoShare/PhotoShare.Client/Core/Commands/LoginUserCommand.cs
@@ -0,0 +1,30 @@
+﻿namespace PhotoShare.Client.Core.Commands
+{
+    using System;
+    using Services;
+    using System.Linq;
+
+    public class LoginUserCommand
+    {
+        private UserService userService;
+
+        public LoginUserCommand(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public string Execute(string[] data)
+        {
+            string username = data[0];
+            string password = data[1];
+            //!password.Any(c => char.IsLower(c)) && password.Any(c => char.IsDigit(c)))
+            if (!this.userService.HasUser(username, password))
+            {
+                throw new ArgumentException("Invalid username or password!");
+            }
+            AuthenticationManager.Login(this.userService.GetUserByUsername(username));
+
+            return $"User {username} logged in!";
+        }
+    }
+}
