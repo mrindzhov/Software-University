4788b55db7ff5b37cd633793a5fdf5e5780ac6f1
Martin <mrindzhov@gmail.com>
2017-06-01 07:16:19 +0300

    Commit progress

---
+++ BasicTreeStructures/Trees/Trees/Tree.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+    public T Value { get; set; }
+
+    public IList<Tree<T>> Children { get; set; }
+
+    public Tree(T value, params Tree<T>[] children)
+    {
+        this.Value = value;
+        this.Children = new List<Tree<T>>();
+        foreach (var child in children)
+        {
+            this.Children.Add(child);
+        }
+    }
+
+    public void Print(int indent = 0)
+    {
+        Console.Write(new string(' ', 2 * indent));
+        Console.WriteLine(this.Value);
+        foreach (var child in this.Children)
+        {
+            child.Print(indent + 1);
+        }
+    }
+
+    public void Each(Action<T> action)
+    {
+        action(this.Value);
+        foreach (var child in this.Children)
+        {
+            child.Each(action);
+        }
+    }
+
+    public IEnumerable<T> OrderDFS()
+    {
+        List<T> result = new List<T>();
+
+        DFS(this, result);
+
+        return result;
+    }
+
+    private void DFS(Tree<T> tree, List<T> result)
+    {
+
+        foreach (var child in tree.Children)
+        {
+            DFS(child, result);
+        }
+        result.Add(tree.Value);
+    }
+
+    public IEnumerable<T> OrderBFS()
+    {
+        List<T> result = new List<T>();
+        Queue<Tree<T>> queue = new Queue<Tree<T>>();
+        queue.Enqueue(this);
+        while (queue.Count > 0)
+        {
+            Tree<T> tree = queue.Dequeue();
+            result.Add(tree.Value);
+
+            foreach (var child in tree.Children)
+            {
+                queue.Enqueue(child);
+            }
+        }
+        return result;
+    }
+}
